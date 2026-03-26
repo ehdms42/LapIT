4de9d318ca56a23a2769b32194690d6603728a27
Doeun <qwaszx080402@naver.com>
2026-03-26 14:59:57 +0900

    ✨ :: 공유결합 개념 페이지 구현

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import IonicConcept from './pages/IonicConcept'
 import IonicBonding from './pages/IonicBonding'
+import { CovalentConcept } from './pages/CovalentConcept'
 
 
 export default function AppRoutes() {
@@ -12,7 +13,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/ionic-concept" element={<IonicConcept />} />
         <Route path="/ionic-lab" element={<IonicBonding />} />
-        <Route path="/covalent-concept" element={<div>공유 개념</div>} />
+        <Route path="/covalent-concept" element={<CovalentConcept />} />
         <Route path="/covalent-lab" element={<div>공유 실습</div>} />
         <Route path="/quiz" element={<div>퀴즈</div>} />
       </Route>
